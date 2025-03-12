57be416580006941e0b029cecf13b6ccc12bfe94
RuBoah <ruboahene@gmail.com>
2025-03-12 11:08:55 +0000

    major changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,57 @@
 import React from "react";
+import { Link } from "react-router";
+import { useState, useEffect } from "react";
+
 
 const NavBar = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <nav className="flex flex-row justify-between px-20 py-6 fixed top-0 left-0 w-full">
+    <nav   className={`fixed top-0 left-0 w-full flex flex-row justify-between px-10 md:px-20 py-4 transition-all duration-300 ${
+      scrolling ? "bg-white shadow-md" : "bg-transparent"
+    }`}
+  >
       <div className=" flex items-center justify-between">
         <h1 className="text-xl font-bold text-blue-950">EdenBOOKS</h1>
       </div>
       <div className="md:flex gap-6 items-center text-blue-950">
-        <span>Home</span>
-        <span>All Books</span>
-        <span>Add Book</span>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/allbooks"}>All Books</Link>
+        <Link to={"/addbooks"}>Add Book</Link>
+<div className="flex items-center border border-blue-900 rounded-lg"> 
         <input
           type="text"
           placeholder="Search Book"
-          className=" border border-blue-950 p-2 rounded"
+          className="   rounded outline-none w-full  "
         />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-5  my-1.5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+</div>
+       
         <button className="px-6 py-1.5 rounded-lg text-white bg-blue-950">
           LOG IN
         </button>
@@ -24,3 +61,4 @@ const NavBar = () => {
 };
 
 export default NavBar;
+// outline-none w-full bg-transparent
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,137 @@ import React from "react";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-
+import { Link } from "react-router";
+import mockingbird from "../assets/images/mockingbird.png";
+import gameofthrones1 from "../assets/images/gameofthrones1.png";
+import annefrank from "../assets/images/annefrank.png";
+import bookthief from "../assets/images/bookthief.png";
 
 const Home = () => {
+  
   return (
     <div>
       <NavBar />
       <section className="mt-16">
-<div className="bg-[url(assets/images/heropic.png)] h-[500px] bg-cover bg-center">
-<h1 className="flex ">Welcome to EdenBOOKS | Your Smart Library Companion 
-</h1>
-<h2>Effortless Library Management, Anytime, Anywhere</h2>
-</div>
+        <div className="bg-[url(assets/images/heropic.png)] h-[500px] bg-cover bg-center flex items-center ps-10">
+          <div>
+            <h1 className="text-2xl font-bold  text-blue-950">
+              Welcome to EdenBOOKS | Your Smart Library Companion
+            </h1>
+            <span className="block mt-2 text-blue-950 ">
+              Effortless Library Management, Anytime, Anywhere
+            </span>
+          </div>
+        </div>
       </section>
-      <Footer/>
+      <section className="">
+        <h1 className="text-3xl font-bold text-center text-blue-950 py-15">
+          Popular Books
+        </h1>
+        <div className="grid grid-cols-4 py-5 ps-8">
+          <div className="bg-blue-950 text-white shadow-lg rounded-lg p-4 hover:scale-105 transition-transform h-[400px] w-[300px]">
+            <h3 className="text-lg font-semibold text-white">
+              Title: A Game of Thrones
+            </h3>
+            <p className="text-white">by George R.R. Martin</p>
+            <img
+              src={gameofthrones1}
+              alt="Gameofthrones"
+              className="h-[300px] w-[300px] object-cover rounded-md mb-4"
+            />
+          </div>
+          <div className="bg-blue-950 text-white shadow-lg rounded-lg p-4 hover:scale-105 transition-transform h-[400px] w-[300px]">
+            <h3 className="text-lg font-semibold text-white">
+              Title: To Kill a Mockingbird
+            </h3>
+            <p className="text-white">by Harper Lee</p>
+            <img
+              src={mockingbird}
+              alt="Mockingbird"
+              className="h-[300px] w-[300px] object-cover rounded-md mb-4"
+            />
+            ;
+          </div>
+          <div className="bg-blue-950 text-white shadow-lg rounded-lg p-4 hover:scale-105 transition-transform h-[400px] w-[300px]">
+            <h3 className="text-lg font-semibold text-white">
+              Title: The Diary of a Young Girl
+            </h3>
+            <p className="text-white">by Anne Frank</p>
+            <img
+              src={annefrank}
+              alt="annefrank"
+              className="h-[300px] w-[300px] object-cover rounded-md mb-4"
+            />
+          </div>
+          <div className="bg-blue-950 text-white shadow-lg rounded-lg p-4 hover:scale-105 transition-transform h-[400px] w-[300px]">
+            <h3 className="text-lg font-semibold text-white">
+              Title: The Book Thief
+            </h3>
+            <p className="text-white">by Markus Zusak</p>
+            <img
+              src={bookthief}
+              alt="Bookthief"
+              className="h-[300px] w-[300px] object-cover rounded-md mb-4"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center py-10">
+          <Link to="/allbooks">
+            <button className="px-6 py-1.5 rounded-lg text-white bg-blue-950">
+              VIEW ALL BOOKS
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      <section className="py-16 bg-gray-100">
+        <div className="max-w-6xl mx-auto px-6 text-center">
+          <h2 className="text-3xl font-bold text-blue-950 mb-8">
+            How It Works
+          </h2>
+
+         
+
+          <div className="grid md:grid-cols-4 gap-8">
+
+          <div className="bg-white p-6 shadow-lg rounded-lg">
+              <div className="text-4xl text-blue-950">🔎</div>
+              <h3 className="text-lg font-semibold mt-4">Search & Read</h3>
+              <p className="text-gray-600 text-sm mt-2">
+                Find books easily and read them with one click.
+              </p>
+            </div>
+            
+            <div className="bg-white p-6 shadow-lg rounded-lg">
+              <div className="text-4xl text-blue-950">📌</div>
+              <h3 className="text-lg font-semibold mt-4">Sign Up & Log In</h3>
+              <p className="text-gray-600 text-sm mt-2">
+                Create an account to enjoy benefits in the library system.
+              </p>
+            </div>
+
+            <div className="bg-white p-6 shadow-lg rounded-lg">
+              <div className="text-4xl text-blue-950">📚</div>
+              <h3 className="text-lg font-semibold mt-4">Add Books</h3>
+              <p className="text-gray-600 text-sm mt-2">
+                Upload book details and in your collection.
+              </p>
+            </div>
+
+        
+
+            <div className="bg-white p-6 shadow-lg rounded-lg">
+              <div className="text-4xl text-blue-950">✅</div>
+              <h3 className="text-lg font-semibold mt-4">Track & Return</h3>
+              <p className="text-gray-600 text-sm mt-2">
+                Keep track of borrowed books and return them on time.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 };
