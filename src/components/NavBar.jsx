f2e60e65f22ad4434ae6994f3a92fc41fffeb79e
RuBoah <ruboahene@gmail.com>
2025-03-12 18:57:54 +0000

    second edit of landing page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router";
 import { useState, useEffect } from "react";
 
-
 const NavBar = () => {
   const [scrolling, setScrolling] = useState(false);
 
@@ -19,10 +18,11 @@ const NavBar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <nav   className={`fixed top-0 left-0 w-full flex flex-row justify-between px-10 md:px-20 py-4 transition-all duration-300 ${
-      scrolling ? "bg-white shadow-md" : "bg-transparent"
-    }`}
-  >
+    <nav
+      className={`h-[5rem] fixed top-0 left-0 w-full flex flex-row justify-between px-10 md:px-20 py-4 transition-all duration-300 ${
+        scrolling ? "bg-white shadow-md" : "bg-transparent"
+      }`}
+    >
       <div className=" flex items-center justify-between">
         <h1 className="text-xl font-bold text-blue-950">EdenBOOKS</h1>
       </div>
@@ -30,35 +30,37 @@ const NavBar = () => {
         <Link to={"/"}>Home</Link>
         <Link to={"/allbooks"}>All Books</Link>
         <Link to={"/addbooks"}>Add Book</Link>
-<div className="flex items-center border border-blue-900 rounded-lg"> 
-        <input
-          type="text"
-          placeholder="Search Book"
-          className="   rounded outline-none w-full  "
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-5  my-1.5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+        <div className="flex items-center border border-blue-900 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search Book"
+            className="   rounded outline-none w-full  "
           />
-        </svg>
-</div>
-       
-        <button className="px-6 py-1.5 rounded-lg text-white bg-blue-950">
-          LOG IN
-        </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5  my-1.5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </div>
+
+        <Link
+          to={"/auth"}
+          className="ml-4 bg-gradient-to-r from-[#056d80] from-10% to-blue-950 to-90% hover:bg-gradient-to-l px-10 py-3 rounded-lg text-white text-center hover:font-extrabold"
+        >
+          Log In
+        </Link>
       </div>
     </nav>
   );
 };
 
 export default NavBar;
-// outline-none w-full bg-transparent
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,23 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { Link } from "react-router";
 import mockingbird from "../assets/images/mockingbird.png";
-import gameofthrones1 from "../assets/images/gameofthrones1.png";
+import thronespic2 from "../assets/images/thronespic2.png";
 import annefrank from "../assets/images/annefrank.png";
 import bookthief from "../assets/images/bookthief.png";
+import { FaSearch, FaUserLock, FaBook } from "react-icons/fa";
+
 
 const Home = () => {
-  
   return (
     <div>
       <NavBar />
-      <section className="mt-16">
-        <div className="bg-[url(assets/images/heropic.png)] h-[500px] bg-cover bg-center flex items-center ps-10">
+      <section className="mt-16 ">
+        <div className="bg-[url(assets/images/heropic1.png)] h-[700px] bg-cover bg-center flex items-center ps-10">
           <div>
-            <h1 className="text-2xl font-bold  text-blue-950">
-              Welcome to EdenBOOKS | Your Smart Library Companion
+            <h1 className="text-4xl font-bold  text-white animate-bounce">
+              Welcome to EdenBOOKS <br /> Your Smart Library Companion
             </h1>
-            <span className="block mt-2 text-blue-950 ">
+            <span className="block mt-2 text-white text-2xl ">
               Effortless Library Management, Anytime, Anywhere
             </span>
           </div>
@@ -36,7 +37,7 @@ const Home = () => {
             </h3>
             <p className="text-white">by George R.R. Martin</p>
             <img
-              src={gameofthrones1}
+              src={thronespic2}
               alt="Gameofthrones"
               className="h-[300px] w-[300px] object-cover rounded-md mb-4"
             />
@@ -78,60 +79,98 @@ const Home = () => {
         </div>
 
         <div className="flex justify-center items-center py-10">
-          <Link to="/allbooks">
-            <button className="px-6 py-1.5 rounded-lg text-white bg-blue-950">
-              VIEW ALL BOOKS
-            </button>
+          <Link
+            to={"/book"}
+            className="ml-4 bg-gradient-to-r from-[#056d80] from-10% to-blue-950 to-90% hover:bg-gradient-to-l px-10 py-3 rounded-lg text-white text-center hover:font-extrabold"
+          >
+            View More Books
           </Link>
         </div>
       </section>
 
-      <section className="py-16 bg-gray-100">
-        <div className="max-w-6xl mx-auto px-6 text-center">
-          <h2 className="text-3xl font-bold text-blue-950 mb-8">
-            How It Works
-          </h2>
+      <section className="bg-gray-200 py-12 px-6">
+      <div className="max-w-5xl mx-auto text-center">
+        <h2 className="text-3xl font-bold text-blue-950 mb-6">What People Say</h2>
+        
+        <div className="grid md:grid-cols-3 gap-8">
+        <div className="bg-white p-6 rounded-2xl shadow-md">
+            <p className="text-gray-700 italic">
+              "Great library App"
+            </p>
+            <div className="flex items-center mt-4">
+             
+              <div>
+                <h4 className="text-gray-900 font-semibold">William King</h4>
+                <p className="text-sm text-gray-500">Senior Designer</p>
+              </div>
+            </div>
+          </div>
+        
+          <div className="bg-white p-6 rounded-2xl shadow-md">
+            <p className="text-gray-700 italic">
+              "Working with EdenBooks was a fantastic experience. Their attention to detail and design skills are top-notch!"
+            </p>
+            <div className="flex items-center mt-4">
+             
+              <div>
+                <h4 className="text-gray-900 font-semibold">Patterson</h4>
+                <p className="text-sm text-gray-500">Senior Designer</p>
+              </div>
+            </div>
+          </div>
 
          
-
-          <div className="grid md:grid-cols-4 gap-8">
-
-          <div className="bg-white p-6 shadow-lg rounded-lg">
-              <div className="text-4xl text-blue-950">🔎</div>
-              <h3 className="text-lg font-semibold mt-4">Search & Read</h3>
-              <p className="text-gray-600 text-sm mt-2">
-                Find books easily and read them with one click.
-              </p>
-            </div>
-            
-            <div className="bg-white p-6 shadow-lg rounded-lg">
-              <div className="text-4xl text-blue-950">📌</div>
-              <h3 className="text-lg font-semibold mt-4">Sign Up & Log In</h3>
-              <p className="text-gray-600 text-sm mt-2">
-                Create an account to enjoy benefits in the library system.
-              </p>
-            </div>
-
-            <div className="bg-white p-6 shadow-lg rounded-lg">
-              <div className="text-4xl text-blue-950">📚</div>
-              <h3 className="text-lg font-semibold mt-4">Add Books</h3>
-              <p className="text-gray-600 text-sm mt-2">
-                Upload book details and in your collection.
-              </p>
-            </div>
-
-        
-
-            <div className="bg-white p-6 shadow-lg rounded-lg">
-              <div className="text-4xl text-blue-950">✅</div>
-              <h3 className="text-lg font-semibold mt-4">Track & Return</h3>
-              <p className="text-gray-600 text-sm mt-2">
-                Keep track of borrowed books and return them on time.
-              </p>
+          <div className="bg-white p-6 rounded-2xl shadow-md">
+            <p className="text-gray-700 italic">
+              "EdenBooks is a skilled and dedicated developer. Their work truly speaks for itself."
+            </p>
+            <div className="flex items-center mt-4">
+             
+              <div>
+                <h4 className="text-gray-900 font-semibold">RAB</h4>
+                <p className="text-sm text-gray-500">Tech Lead</p>
+              </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
+
+    <section className="py-16 bg-gray-100">
+      <div className="max-w-6xl mx-auto text-center px-6">
+        <h2 className="text-3xl font-bold text-blue-950 mb-10">
+          Explore Features
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+        
+          <div className="bg-white p-8 shadow-lg rounded-lg flex flex-col items-center">
+            <FaSearch className="text-5xl text-blue-950" />
+            <h3 className="text-lg font-semibold mt-4">Search</h3>
+            <p className="text-gray-600 text-sm mt-2 text-center">
+              Find books easily and read them with one click.
+            </p>
+          </div>
+
+      
+          <div className="bg-white p-8 shadow-lg rounded-lg flex flex-col items-center">
+            <FaUserLock className="text-5xl text-blue-950" />
+            <h3 className="text-lg font-semibold mt-4">Log In</h3>
+            <p className="text-gray-600 text-sm mt-2 text-center">
+              Create an account to enjoy benefits in the library system.
+            </p>
+          </div>
+
+        
+          <div className="bg-white p-8 shadow-lg rounded-lg flex flex-col items-center">
+            <FaBook className="text-5xl text-blue-950" />
+            <h3 className="text-lg font-semibold mt-4">Add Books</h3>
+            <p className="text-gray-600 text-sm mt-2 text-center">
+              Upload books to your collection.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
       <Footer />
     </div>
   );
